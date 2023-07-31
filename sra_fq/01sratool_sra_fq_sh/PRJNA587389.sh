760ff88a034f5a8ccebfe44a3049f2e8a7f6b9c5
YangJingqii <yangjingqi_work@163.com>
2023-07-31 20:32:34 +0800

    Add sra_fq

---
+++ sra_fq/01sratool_sra_fq_sh/PRJNA587389.sh
@@ -0,0 +1,1 @@
+/mnt/alamo01/users/yangjingqi/app/sratoolkit/sratoolkit.3.0.5-centos_linux64/bin/fastq-dump --gzip --split-3 -X 25000 -O /mnt/alamo01/users/yangjingqi/RSV_sra/group/fastq_result/PRJNA587389 /mnt/alamo01/users/yangjingqi/RSV_sra/yjq_sra_bioproject_rsv/PRJNA587389/SRR10390627
